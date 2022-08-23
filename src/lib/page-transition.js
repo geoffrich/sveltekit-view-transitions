20aec09a22bcfe0b9dad088657aa616df296ef9e
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-08-23 16:42:25 -0700

    Use updated transition hook

--- src/lib/page-transition.js
+++ src/lib/page-transition.js
@@ -21,7 +21,7 @@ function getNavigationStore() {
 		if (n === null) {
 			while (callbacks.length > 0) {
 				const res = callbacks.pop();
-				res();
+				res?.();
 			}
 		}
 	});
@@ -33,26 +33,79 @@ function getNavigationStore() {
 	return navigation;
 }
 
-export const preparePageTransition = () => {
+/**
+ * @callback pageTransitionCallback
+ * @param {{ from: URL, to: URL, type: TransitionType}} nav
+ */
+
+const beforeCallbacks = new Set(); // before transition starts
+const afterCallbacks = new Set(); // after transition has completed
+const incomingCallbacks = new Set(); // when new page is loaded but transition has not completed
+
+/**
+ * @param {pageTransitionCallback} fn
+ */
+export const beforePageTransition = (fn) => {
+	beforeCallbacks.add(fn);
+
+	onDestroy(() => {
+		beforeCallbacks.delete(fn);
+	});
+};
+
+/**
+ * @param {pageTransitionCallback} fn
+ */
+export const whileIncomingTransition = (fn) => {
+	incomingCallbacks.add(fn);
+
+	onDestroy(() => {
+		incomingCallbacks.delete(fn);
+	});
+};
+
+/**
+ * @param {pageTransitionCallback} fn
+ */
+export const afterPageTransition = (fn) => {
+	afterCallbacks.add(fn);
+
+	onDestroy(() => {
+		afterCallbacks.delete(fn);
+	});
+};
+
+/**
+ * @param {(from: string, to: string) => string?} getType
+ */
+export const preparePageTransition = (getType = (_from, _to) => null) => {
 	const navigation = getNavigationStore();
-	let isReducedMotionEnabled;
+	let isReducedMotionEnabled = false;
 
 	let unsubReducedMotion = reducedMotion.subscribe((val) => (isReducedMotionEnabled = val));
 
 	// before navigating, start a new transition
-	beforeNavigate(() => {
+	beforeNavigate(({ from, to }) => {
 		// Feature detection
 		if (!document.createDocumentTransition || isReducedMotionEnabled) {
 			return;
 		}
 
+		const type = getType(from.pathname, to?.pathname ?? '');
 		try {
 			const transition = document.createDocumentTransition();
+			const payload = { from, to, type };
+			beforeCallbacks.forEach((fn) => fn(payload));
 			// init before transition.start so the promise doesn't resolve early
 			const navigationComplete = navigation.complete();
-			transition.start(async () => {
-				await navigationComplete;
-			});
+			transition
+				.start(async () => {
+					await navigationComplete;
+					incomingCallbacks.forEach((fn) => fn(payload));
+				})
+				.then(() => {
+					afterCallbacks.forEach((fn) => fn(payload));
+				});
 		} catch (e) {
 			// without the catch, we could throw in beforeNavigate and prevent navigation
 			console.error(e);
